95707e135f371de9b0f92d596bb024f16fd184b7
Nikolay <Kreicer0@gmail.com>
2025-10-14 10:00:57 +0300

    дом. задание Функции еще испр-я

---
+++ 7.async/my_asyncObj.js
@@ -0,0 +1,26 @@
+
+const user = {
+    name: 'Лев Толстой',
+    login: 'leo_tolstoy',
+    reputation: 97,
+    total: 900,
+ }
+
+ let name = 'name';
+ let reputation = 'reputation';
+ let total = 'total';
+
+console.log(`${user[name]}, репутация ${user[reputation]}, сообщений ${user[total]}` )
+
+
+const AlarmClock = {
+    constructor: alarmCollection = [],
+    intervalId: id = 0,
+    addClock(time, canCall) {
+        // let time;
+        // let canCall = true;
+    },
+    removeClock(time) {
+        
+    }
+}
--- 7.async/task.js
+++ 7.async/task.js
@@ -1,14 +1,4 @@
 
-// let alarmCollection = [];
-// let id;
-
-// const AlarmClock = {
-//     constructor: alarmCollection,
-//     intervalId: id
-// }
-// const addClock = {
-
-// }
 
 let messages = [
   ["zloy-zloy", "Мы с Алешей сегодня на иксбоксе рубились, потом в маке поели, потом ему новый телефон купили, и два часа с горок в Нескучном саду катались."],
@@ -19,13 +9,35 @@ let messages = [
 
 let newMessages = [];
 
+// newMessages[0] = {};
+// newMessages[0].author = messages[0][0];
+// newMessages[0].text = messages[0][1];
+
+// newMessages[1] = {};
+// newMessages[1].author = messages[1][0];
+// newMessages[1].text = messages[1][1];
+
+// newMessages[2] = {};
+// newMessages[2].author = messages[2][0];
+// newMessages[2].text = messages[2][1];
+
+// newMessages[3] = {};
+// newMessages[3].author = messages[3][0];
+// newMessages[3].text = messages[3][1];
+
 for(let i = 0; i < messages.length; i++) {
+  for(let j = 0; j < messages[i].length - 1; j++) {
     newMessages[i] = {};
-    for(let j = 0; j < 1; j++) {
-        newMessages[0].author = messages[i][j];
-        newMessages[0].text = messages[i][j + 1];
-    };
-    console.log('newMessages - ', newMessages, typeof(newMessages));
-    messages.push(newMessages)
+    newMessages[i].author = messages[i][j];
+    newMessages[i].text = messages[i][j + 1];
+  };
+  
 };
-console.log(messages);
+console.log('newMessages - ', newMessages);
+
+// const now = new Date();
+// let hours = String(now.getHours()).padStart(2, '0');
+// let minutes = String(now.getMinutes()).padStart(2, '0');
+// const timeString = `${hours}:${minutes}`;
+// console.log(timeString);
+
